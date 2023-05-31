656d08f8d83b6704a895159e00a48fac30bc60b5
Timur Toktoshev <timofeytk@yandex.ru>
2023-05-31 16:52:42 +0600

    header, footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <ul class="navbar-list">
+        <li class="navbar-item" v-for="link in links" :key="link.alias">
+          <router-link class="navbar-link" :to="link.url">{{
+              link.title
+            }}</router-link>
+        </li>
+      </ul>
+      <!-- link to course -->
+      <ul class="navbar-list" style="margin-top: 14px;">
+        <li class="navbar-item" style="opacity: 0.6; font-size: 14px;">
+          <a
+              href="https://github.com/timofeytk"
+              class="navbar-link"
+          >Профиль на гитхабе</a
+          >
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+import {links} from "@/_config";
+
+export default {
+  data() {
+    return {
+      links: links,
+    };
+  },
+};
+</script>
